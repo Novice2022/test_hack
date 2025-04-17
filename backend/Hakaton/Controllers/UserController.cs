20b7a12901e7c44358ab0f9a5d8748d8c8207beb
JustHK <lukin.sergey.01@mail.ru>
2025-04-17 21:33:54 +0300

    backend init

---
+++ backend/Hakaton/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Hakaton.DataAccess;
+using Hakaton.Models;
+using Hakaton.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hakaton.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly HakatonDbContext _dbContext;
+        private readonly ILogger<AuthorizationController> _logger;
+
+        public UserController(HakatonDbContext dbContext, ILogger<AuthorizationController> logger)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("getallusers")]
+        [Authorize]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _dbContext.Users.ToListAsync();
+
+            if (users == null || users.Count == 0)
+            {
+                return NotFound("No users found.");
+            }
+
+            return Ok(users);
+        }
+    }
+}
---
+++ backend/Hakaton/DataAccess/HakatonDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Hakaton.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hakaton.DataAccess
+{
+    public class HakatonDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public HakatonDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
